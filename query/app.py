4f85e381510a3794d1388e4bd77677785d1876bb
wesky93 <wesky93@gmail.com>
2020-05-22 23:06:17 +0900

    2020-05-22 기록
    - kendra proxy api 구현
    - s3에 크롤링 문서 적재
    - 켄드라 인덱싱 테스트
    - 단일 켄드라에서 여러 웹사이트 검색 서비스를 제공하는 것으로 결정(멀티테넌시 구현 필요)

---
+++ query/app.py
@@ -0,0 +1,40 @@
+# FIXME: Not sure how to add this in our system
+import boto3
+from flask import Flask, request, abort, jsonify
+
+app = Flask(__name__)
+kendra = boto3.client('kendra')
+
+
+@app.route('/')
+def hello_world():
+    return '<h1>Helllo</h1>'
+
+
+white_list = [
+    'query',
+    'list_indices',
+    'list_data_sources',
+    'list_indices',
+    'list_faqs',
+    'describe_data_source',
+    'describe_faq',
+    'describe_index',
+]
+
+
+@app.route('/kendra/<method>', methods=['POST'])
+def kendra_client(method: str):
+    if method not in white_list:
+        abort(404)
+    try:
+        return jsonify(kendra[method](**request.form))
+    except Exception as e:
+        return jsonify({
+            'error': f"{e}",
+        })
+
+        # We only need this for local development.
+
+if __name__ == '__main__':
+    app.run()
--- scrap/handler.py
+++ scrap/handler.py
@@ -10,7 +10,7 @@ from scrapy.spiders import SitemapSpider
 td = tempfile.TemporaryDirectory()
 BASE_DIR = td.name
 DATA_DIR = os.path.join(BASE_DIR, 'data')
-BUCKET = os.environ.get('BUCKET', 'awskrug.kendra.dev')
+BUCKET = os.environ.get('BUCKET', 'kendra-button')
 
 s3 = boto3.client('s3')
 
@@ -19,12 +19,12 @@ def make_hash(name: str):
     return shortuuid.uuid(name=name)
 
 
-def make_obj_name(site: str, url: str):
-    return f"{make_hash(site)}/{make_hash(url)}"
+def make_obj_name(site: str, url: str,ext:str):
+    return f"{make_hash(site)}/{make_hash(url)}.{ext}"
 
 
-def make_meta_name(site: str, url: str):
-    return f"{make_hash(site)}/meta/{make_hash(url)}.json"
+def make_meta_name(path:str):
+    return f"{path}/metadata.json"
 
 
 class AWSDoc(SitemapSpider):
@@ -41,14 +41,50 @@ class AWSDoc(SitemapSpider):
         file = os.path.join(DATA_DIR, 'aws_doc', service, path)
         os.makedirs(os.path.dirname(file), exist_ok=True)
         buff2 = io.BytesIO(response.body)
-        s3.upload_fileobj(buff2, BUCKET, make_obj_name(self.name, response.url))
-        # s3.upload_fileobj(buff2, BUCKET, make_meta_name(self.name, response.url))
+        obj_path = make_obj_name(self.name, response.url,'html')
+        s3.upload_fileobj(buff2, BUCKET, obj_path)
+#         metadata = {
+#             "DocumentId": "document ID",
+#             "Attributes": {
+#         "_category": "document category",
+#         "_created_at": "ISO 8601 encoded string",
+#         "_last_updated_at": "ISO 8601 encoded string",
+#         "_source_uri": "document URI,
+#         "_version": "file version",
+#         "_view_count": number of times document has been viewed,
+#         "custom attribute key": "custom attribute value",
+#         additional custom attributes
+#     },
+#     "AccessControlList": [
+#          {
+#              "Name": "user name",
+#              "Type": "GROUP | USER",
+#              "Access": "ALLOW | DENY"
+#          }
+#     ],
+#     "Title": "document title",
+#     "ContentType": "HTML | MS_WORD | PDF | PLAIN_TEXT | PPT"
+# }
+        # s3.upload_fileobj(buff2, BUCKET, make_meta_name(obj_path))
 
         yield {
             "url": response.url,
             "service": service,
         }
+class MIT_SITE(SitemapSpider):
+    name = 'aws_doc'
+    base_path = 'https://docs.aws.amazon.com/'
+    sitemap_urls = ['https://docs.aws.amazon.com/kendra/latest/dg/sitemap.xml']
 
+    async def parse(self, response):
+        print(response.url)
+        doc_path = response.url[len(self.base_path):].split('/')
+        service = doc_path[0]
+        path = '-'.join(doc_path[1:])
+
+        buff2 = io.BytesIO(response.body)
+        obj_path = make_obj_name(self.name, response.url,'html')
+        s3.upload_fileobj(buff2, BUCKET, obj_path)
 
 def handler(event, context):
     td = tempfile.TemporaryDirectory()
@@ -64,7 +100,8 @@ def handler(event, context):
         }
     })
 
-    process.crawl(AWSDoc)
+    # process.crawl(AWSDoc)
+    process.crawl(MIT_SITE)
     process.start(stop_after_crawl=True)  # the script will block here until the crawling is finished
 
 
