0972803ce1e374643172242f2d58f1d6e82d66b9
Sung Kim <hunkim@gmail.com>
2020-05-27 21:21:57 +0900

    Updated http

--- query/tests/query_test.py
+++ query/tests/query_test.py
@@ -3,29 +3,24 @@ import requests
 
 import requests
 
-url = "https://0y3n5xq4vi.execute-api.us-west-2.amazonaws.com/dev/kendra/"
 
-
-files = [
-
-]
-headers= {}
-
-response = requests.request("POST", url, headers=headers, data = payload, files = files)
-
-print(response.text.encode('utf8'))
 
 
 class QueryPOSTTestCase(unittest.TestCase):
     """ Test for the Query """ 
-    lambda_url = "https://0y3n5xq4vi.execute-api.us-west-2.amazonaws.com/dev"
+    url = "https://0y3n5xq4vi.execute-api.us-west-2.amazonaws.com/dev/kendra/"
 
     def test_query_post(self):
         
         """ request POST """ 
         # Fire POST
-        payload = {'IndexId': '65a73c7f-74df-43da-950b-70cbc46be778',
-            'QueryText': 'kendra'}
-        actual = requests.post(lambda_url + "/query", data = query)
+        payload = {"IndexId": "65a73c7f-74df-43da-950b-70cbc46be778",
+                  "QueryText": "kendra"}
+        expected = {}
+
+        actual = requests.post(self.url + "query", data = payload)
+        print(actual.text.encode('utf8'))
 
         self.assertEqual(expected, actual)
+
+unittest.main()
