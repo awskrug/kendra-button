3f77e2d9a6fa99e6661f5ad5338ec32048d86d3b
TaeSeong Park <imf4@naver.com>
2020-05-28 23:26:39 +0900

    attach amplify framework and ui library

--- frontend/kendra-button-front/pages/index.tsx
+++ frontend/kendra-button-front/pages/index.tsx
@@ -1,6 +1,11 @@
-import { useCallback, useState } from 'react'
+import {
+  AmplifySignOut,
+  AmplifySignUp,
+  withAuthenticator,
+} from '@aws-amplify/ui-react';
+import { useCallback, useState } from 'react';
 
-type stateBtype = string|number;
+type stateBtype = string | number;
 interface BText {
   text: string;
   state: any;
@@ -8,13 +13,18 @@ interface BText {
 }
 
 const Button = (props: BText) => {
-  const { text, state, stateB } = props
-  const btnOnClick = useCallback((e: any) => {
-    alert('button clicked!' + stateB)
-  }, [state])
+  const { text, state, stateB } = props;
+  const btnOnClick = useCallback(
+    (e: any) => {
+      alert('button clicked!' + stateB);
+    },
+    [state],
+  );
   return (
     <>
-      <button className={'btn'} onClick={btnOnClick}>{text}</button>
+      <button className={'btn'} onClick={btnOnClick}>
+        {text}
+      </button>
       {/* css in js */}
       <style jsx>{`
         .btn {
@@ -22,33 +32,35 @@ const Button = (props: BText) => {
         }
       `}</style>
     </>
-  )
-}
+  );
+};
 
-const Index = props => {
-  console.log({ props })
-  const [state, setState] = useState(null)
-  const [stateB, setStateB] = useState<stateBtype>(null)
+const Index = (props) => {
+  console.log({ props });
+  const [state, setState] = useState(null);
+  const [stateB, setStateB] = useState<stateBtype>(null);
   return (
     <>
       <h1>Nice to meet ya!</h1>
       <h2>This is Kendra-Frontend</h2>
-      <p id="cool">
-        Cool!
-      </p>
+      <p id='cool'>Cool!</p>
       <Button text={'this is button'} state={state} stateB={stateB} />
-      <div>
-        <img src="https://files.slack.com/files-pri/T08A93WLQ-F013ZRDJ9U4/image.png" 
-      width="100%"/>
-      </div>
       <style jsx>{`
         #cool {
           background-color: orange;
         }
-
       `}</style>
     </>
-  )
-}
+  );
+};
 
-export default Index
+// export default Index
+
+const App = () => (
+  <div>
+    <AmplifySignOut />
+    My App
+  </div>
+);
+
+export default App
