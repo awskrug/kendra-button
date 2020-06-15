ab1202286fbe7c270a77cd6bac9d410115dd5304
TaeSeong Park <imf4@naver.com>
2020-06-16 02:13:48 +0900

    Temporarily update endpoint to noauth

--- frontend/kendra-button-front/src/pages/_app.tsx
+++ frontend/kendra-button-front/src/pages/_app.tsx
@@ -13,15 +13,17 @@ import awsconfig from '../aws-exports';
 
 Amplify.configure(awsconfig);
 
-/** 
+/**
  * Using a non-AppSync GraphQL Server
  * @tutorial https://docs.amplify.aws/lib/graphqlapi/create-or-re-use-existing-backend/q/platform/js#using-a-non-appsync-graphql-server amplify
-*/
+ */
 Amplify.configure({
   API: {
-    graphql_endpoint: 'https://8pfums0mu7.execute-api.us-west-2.amazonaws.com/dev/graphql',
-  }
-})
+    // graphql_endpoint: 'https://8pfums0mu7.execute-api.us-west-2.amazonaws.com/dev/graphql', // dev
+    graphql_endpoint:
+      'https://8pfums0mu7.execute-api.us-west-2.amazonaws.com/dev/noauth/graphql', // noauth
+  },
+});
 
 function App({ Component, pageProps }: AppProps) {
   return (
