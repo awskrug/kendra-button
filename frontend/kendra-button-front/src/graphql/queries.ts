86fd30dbc77f4db43ef5faec7589b5a557a4cffd
TaeSeong Park <imf4@naver.com>
2020-06-16 02:13:03 +0900

    Update queries (createSite, sites)

--- frontend/kendra-button-front/src/graphql/queries.ts
+++ frontend/kendra-button-front/src/graphql/queries.ts
@@ -1,18 +1,35 @@
-export const createSite = `mutation CreateSite{
-  createSite(site:"asdfasdfa",title:"vd",url:"https://avcd.com"){
-    site{
+export const createSite = `mutation Create(
+  $site: String!
+  $title: String!
+  $url: String!
+) {
+  createSite(
+    site: $site
+    title: $title
+    url: $url
+  ) {
+    site {
+      site
       title
       url
       user
+      crawlerStatus {
+        total
+        done
+      }
     }
   }
 }`;
 
 export const siteList = `query SiteList{
 	sites{
-      site
-      user
-      title
-      url
-  }  
+    site
+    user
+    title
+    url
+    crawlerStatus {
+      total
+      done
+    }
+  }
 }`;
