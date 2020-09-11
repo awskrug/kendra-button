1fc7be7e1f27b02ab4067ebaf7ed34e8d98c05cd
TaeSeong Park <imf4@naver.com>
2020-09-12 00:01:34 +0900

    Backup logic implemented

--- cognito/backup.ts
+++ cognito/backup.ts
@@ -1,40 +1,56 @@
-// const AWS = require('aws-sdk');
-
 import * as AWS from 'aws-sdk';
+
+const localProfileName = 'kendra-geoseong';
+
+const creds = new AWS.SharedIniFileCredentials({ profile: localProfileName });
+AWS.config.region = 'us-west-2';
+AWS.config.credentials = creds;
 const cognitoIdp = new AWS.CognitoIdentityServiceProvider();
 
 type CognitoISP = AWS.CognitoIdentityServiceProvider;
+type UserType = AWS.CognitoIdentityServiceProvider.UserType;
 type ListUsersRequestTypes = AWS.CognitoIdentityServiceProvider.Types.ListUsersRequest;
+type ListUsersResponseType = AWS.CognitoIdentityServiceProvider.UsersListType;
 type AdminCreateUserRequest = AWS.CognitoIdentityServiceProvider.Types.AdminCreateUserRequest;
 type AttributeType = AWS.CognitoIdentityServiceProvider.Types.AttributeType;
 
-const getUserByEmail = async (userPoolId, email) => {
-  const params = {
-    UserPoolId: userPoolId,
-    Filter: `email = "${email}"`,
-  };
-  return cognitoIdp.listUsers(params).promise();
-};
+interface BackupReqProp {
+  userData: ListUsersResponseType;
+  params: ListUsersRequestTypes;
+}
+const backupUserpoolData = async (
+  backupReqProp: BackupReqProp,
+): Promise<ListUsersResponseType> => {
+  const {
+    Users = [] as UserType[],
+    PaginationToken,
+  } = await cognitoIdp.listUsers(backupReqProp.params).promise();
 
-const userRs = await getUserByEmail(
-  event.userPoolId,
-  event.request.userAttributes.email,
-);
-const params: ListUsersRequestTypes = {
-  UserPoolId: poolId,
-};
-
-const paginationCalls = async () => {
-  const { Users = [], PaginationToken } = await cognito
-    .listUsers(params)
-    .promise();
-  Users.forEach((user) => stringify.write(user as string));
+  const existingUserData = backupReqProp.userData;
+  const newUserData = [...existingUserData, ...Users] as ListUsersResponseType;
 
   if (PaginationToken) {
-    params.PaginationToken = PaginationToken;
-    if (delayDurationInMillis > 0) {
-      await delay(delayDurationInMillis);
-    }
-    await paginationCalls();
+    return await backupUserpoolData({
+      userData: newUserData,
+      params: {
+        UserPoolId: backupReqProp.params.UserPoolId,
+        PaginationToken: backupReqProp.params.PaginationToken,
+      },
+    });
+  } else {
+    /* Userpool의 Users[] 을 리턴 */
+    return newUserData;
   }
 };
+/*
+[backup/restore]
+backup Userpool -> target Userpool
+*/
+backupUserpoolData({
+  userData: [] as UserType[],
+  params: {
+    UserPoolId: 'us-west-2_XT1s3RtPp',
+  },
+}).then((userdata: ListUsersResponseType) => {
+  console.log('backup userinfo', JSON.stringify(userdata, null, 2));
+});
