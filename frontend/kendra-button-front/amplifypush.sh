7109753456e33a7a121ddf914ca360187dcedc9b
TaeSeong Park <imf4@naver.com>
2020-09-18 22:59:10 +0900

    Update manual scripts refer to https://github.com/aws-amplify/amplify-console/blob/master/scripts/amplifyPush.sh

--- frontend/kendra-button-front/amplifypush.sh
+++ frontend/kendra-button-front/amplifypush.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 IFS='|'
+
 help_output () {
     echo "usage: amplify-push <--environment|-e <name>> <--simple|-s>"
     echo "  --environment  The name of the Amplify environment to use"
@@ -15,19 +16,20 @@ init_env () {
     CODEGEN=$4
     AWSCONFIG=$5
     CATEGORIES=$6
+
     echo "# Start initializing Amplify environment: ${ENV}"
     if [[ -z ${STACKINFO} ]];
     then
         echo "# Initializing new Amplify environment: ${ENV} (amplify init)"
-        amplify init --amplify ${AMPLIFY} --providers ${PROVIDERS} --codegen ${CODEGEN} --categories ${CATEGORIES} --yes;
+        [[ -z ${CATEGORIES} ]] && amplify init --amplify ${AMPLIFY} --providers ${PROVIDERS} --codegen ${CODEGEN} --yes || amplify init --amplify ${AMPLIFY} --providers ${PROVIDERS} --codegen ${CODEGEN} --categories ${CATEGORIES} --yes
         echo "# Environment ${ENV} details:"
         amplify env get --name ${ENV}
     else
         echo "STACKINFO="${STACKINFO}
         echo "# Importing Amplify environment: ${ENV} (amplify env import)"
-        amplify env import --name ${ENV} --config "${STACKINFO}" --awsInfo ${AWSCONFIG} --categories ${CATEGORIES} --yes;
+        amplify env import --name ${ENV} --config "${STACKINFO}" --awsInfo ${AWSCONFIG} --yes;
         echo "# Initializing existing Amplify environment: ${ENV} (amplify init)"
-        amplify init --amplify ${AMPLIFY} --providers ${PROVIDERS} --codegen ${CODEGEN} --categories ${CATEGORIES} --yes;
+        [[ -z ${CATEGORIES} ]] && amplify init --amplify ${AMPLIFY} --providers ${PROVIDERS} --codegen ${CODEGEN} --yes || amplify init --amplify ${AMPLIFY} --providers ${PROVIDERS} --codegen ${CODEGEN} --categories ${CATEGORIES} --yes
         echo "# Environment ${ENV} details:"
         amplify env get --name ${ENV}
     fi
@@ -38,28 +40,27 @@ ENV=""
 IS_SIMPLE=false
 POSITIONAL=()
 while [[ $# -gt 0 ]]
-do
+    do
     key="$1"
     case ${key} in
         -e|--environment)
-            ENV=$2
-            shift
-            ;;
+        ENV=$2
+        shift
+        ;;
         -r|--region)
-            REGION=$2
-            shift
-            ;;
+        REGION=$2
+        shift
+        ;;
         -s|--simple)
-            IS_SIMPLE=true
-            shift
-            ;;
+        IS_SIMPLE=true
+        shift
+        ;;
         *)
-            POSITIONAL+=("$1")
-            shift
-            ;;
+        POSITIONAL+=("$1")
+        shift
+        ;;
     esac
 done
-
 set -- "${POSITIONAL[@]}"
 
 # if no provided environment name, use default env variable, then user override
@@ -67,10 +68,14 @@ if [[ ${ENV} = "" ]];
 then
     ENV=${AWS_BRANCH}
 fi
+
 if [[ ${USER_BRANCH} != "" ]];
 then
     ENV=${USER_BRANCH}
 fi
+
+# strip slashes, limit to 10 chars
+ENV=$(echo ${ENV} | sed 's;\\;;g' | sed 's;\/;;g' | cut -c -10)
 
 # Check valid environment name
 if [[ -z ${ENV} || "${ENV}" =~ [^a-zA-Z0-9\-]+ ]] ; then help_output ; fi
@@ -78,33 +83,36 @@ if [[ -z ${ENV} || "${ENV}" =~ [^a-zA-Z0-9\-]+ ]] ; then help_output ; fi
 AWSCONFIG="{\
 \"configLevel\":\"project\",\
 \"useProfile\":true,\
-\"profileName\":\"default\"\
+\"profileName\":\"default\",\
+\"AmplifyAppId\":\"${AWS_APP_ID}\"\
 }"
-
 AMPLIFY="{\
-\"envName\":\"${ENV}\"\
+\"envName\":\"${ENV}\",\
+\"appId\":\"${AWS_APP_ID}\"\
 }"
-
 PROVIDERS="{\
 \"awscloudformation\":${AWSCONFIG}\
 }"
-
 CODEGEN="{\
 \"generateCode\":false,\
 \"generateDocs\":false\
 }"
-
-AUTHCONFIG="{\
-\"facebookAppIdUserPool\":\"${FACEBOOK_APP_ID}\",\
-\"facebookAppSecretUserPool\":\"${FACEBOOK_APP_SECRET}\"\
-\"googleAppIdUserPool\":\"${GOOGLE_CLIENT_ID}\",\
-\"googleAppSecretUserPool\":\"${GOOGLE_CLIENT_SECRET}\"\
-}"
-
-CATEGORIES="{\
-\"auth\":$AUTHCONFIG\
-}"
-
+CATEGORIES=""
+if [[ -z ${AMPLIFY_FACEBOOK_CLIENT_ID} && -z ${AMPLIFY_GOOGLE_CLIENT_ID} && -z ${AMPLIFY_AMAZON_CLIENT_ID} ]]; then
+    CATEGORIES=""
+else
+    AUTHCONFIG="{\
+    \"facebookAppIdUserPool\":\"${AMPLIFY_FACEBOOK_CLIENT_ID}\",\
+    \"facebookAppSecretUserPool\":\"${AMPLIFY_FACEBOOK_CLIENT_SECRET}\",\
+    \"googleAppIdUserPool\":\"${AMPLIFY_GOOGLE_CLIENT_ID}\",\
+    \"googleAppSecretUserPool\":\"${AMPLIFY_GOOGLE_CLIENT_SECRET}\",\
+    \"amazonAppIdUserPool\":\"${AMPLIFY_AMAZON_CLIENT_ID}\",\
+    \"amazonAppSecretUserPool\":\"${AMPLIFY_AMAZON_CLIENT_SECRET}\"\
+    }"
+    CATEGORIES="{\
+    \"auth\":$AUTHCONFIG\
+    }"
+fi
 # Handle old or new config file based on simple flag
 if [[ ${IS_SIMPLE} ]];
 then
