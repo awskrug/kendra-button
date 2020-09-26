14f41339a2ca3809eb398ff826894bba946735ce
TaeSeong Park <imf4@naver.com>
2020-09-26 20:46:24 +0900

    Add validation and dev flag

--- frontend/kendra-button-service/utils/fetch.ts
+++ frontend/kendra-button-service/utils/fetch.ts
@@ -1,23 +1,34 @@
-import { getSearchQry } from '../graphql';
+import { getSearchQry, validationQry } from '../graphql';
 interface Props {
   text: string;
   site: string;
+  isValidation?: boolean;
+  dev?: string;
 }
 
-const callGraphql = async ({ text = '', site = '' }: Props): Promise<any> => {
-  const qry = getSearchQry({
-    text,
-    site,
-  });
+const callGraphql = async ({
+  text = '',
+  site = '',
+  isValidation,
+  dev,
+}: Props): Promise<any> => {
+  const qry = isValidation
+    ? validationQry({ site })
+    : getSearchQry({
+        text,
+        site,
+      });
+  console.log('callGraphql', { isValidation, dev });
   console.log('qry:', qry);
-  const res = await fetch(
-    'https://dev.kendra-btns.whatilearened.today/noauth/graphql',
-    {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ query: qry }),
-    }
-  );
+
+  const reqUrl = dev
+    ? 'https://dev.kendra-btns.whatilearened.today/noauth/graphql'
+    : 'https://prod.kendra-btns.whatilearened.today/noauth/graphql';
+  const res = await fetch(reqUrl, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query: qry }),
+  });
   const data = await res.json();
 
   if (data.errors && data.errors.length > 0) {
